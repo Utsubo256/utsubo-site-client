bff4418f2a22a70b0ac4460d74dfa0c19c1bdcc0
Utsubo256 <greeeentea256@gmail.com>
2023-04-19 15:01:42 +0900

    Headerの関数コンポーネントを作成

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,125 @@
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { BiLogOut } from 'react-icons/bi';
+
+const navbarLinks = [
+  { name: 'ホーム', url: '/' },
+  { name: 'ウツボ一覧', url: '/morays' },
+  { name: '水族館一覧', url: '/aquariums' },
+];
+
+export default function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const name = 'Utsubo100';
+  const loading = false;
+  const currentUser = true;
+
+  return (
+    <Box bg={useColorModeValue('blue.500', 'blue.900')} px={4}>
+      <Flex alignItems={'center'} h={16} justifyContent={'space-between'}>
+        <IconButton
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          onClick={isOpen ? onClose : onOpen}
+          size={'md'}
+        />
+        <HStack alignItems={'center'} spacing={8}>
+          <Box>Logo</Box>
+          <HStack as={'nav'} display={{ base: 'none', md: 'flex' }} spacing={4}>
+            {navbarLinks.map((link) => (
+              <Link
+                _hover={{
+                  bg: 'gray.200',
+                  textDecoration: 'none',
+                }}
+                color={'whiteAlpha.900'}
+                href={link.url}
+                key={link.name} // key propsはイテレータ内でどのアイテムかをReactが認識するために使われる
+                px={2}
+                py={1}
+                rounded={'md'}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems={'center'}>
+          <Menu>
+            {!loading && currentUser ? (
+              <>
+                <MenuButton as={Button} cursor={'pointer'} minW={0} rounded={'full'} variant={'link'}>
+                  <Avatar
+                    size={'sm'}
+                    src={
+                      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    }
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem as="a" href="#">
+                    <Stack>
+                      <Text>ログインユーザー</Text>
+                      <Text as="b">{name}</Text>
+                    </Stack>
+                  </MenuItem>
+                  <MenuDivider />
+                  <MenuItem as="a" href="##">
+                    プロフィール編集
+                  </MenuItem>
+                  <MenuDivider />
+                  <MenuItem icon={<BiLogOut />} onClick={() => alert('logout!')}>
+                    ログアウト
+                  </MenuItem>
+                </MenuList>
+              </>
+            ) : (
+              <p>Login</p>
+            )}
+          </Menu>
+        </Flex>
+      </Flex>
+
+      {isOpen ? (
+        <Box display={{ md: 'none' }} pb={4}>
+          <Stack as={'nav'} spacing={4}>
+            {navbarLinks.map((link) => (
+              <Link
+                _hover={{
+                  bg: 'gray.200',
+                  textDecoration: 'none',
+                }}
+                color={'whiteAlpha.900'}
+                href={link.url}
+                key={link.name} // key propsはイテレータ内でどのアイテムかをReactが認識するために使われる
+                px={2}
+                py={1}
+                rounded={'md'}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
+}

417b270de2b7c5f22f19fa60220492609a54ba57
Utsubo256 <greeeentea256@gmail.com>
2023-04-19 15:49:22 +0900

    ビルド時の警告に対応するためchildrenに型を追加

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,12 @@
+import * as React from 'react';
+
 import Header from './Header';
 
-export default function Layout({ children }) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
   return (
     <>
       <Header />

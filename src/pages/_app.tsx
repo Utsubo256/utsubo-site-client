fa6c2d8d5874bec3d4b2e8b3c24dbcef22960c0f
Utsubo256 <greeeentea256@gmail.com>
2023-04-20 12:45:25 +0900

    グローバルステートを保持する機能を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
 import Layout from '@/components/Layout/Layout';
+import { AuthContextProvider } from '@/context/AuthContext';
 
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AuthContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthContextProvider>
     </ChakraProvider>
   );
 }

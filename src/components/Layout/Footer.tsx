ff4b80bd6068e6fcfbc3be159eddada6ea2a47fc
Utsubo256 <greeeentea256@gmail.com>
2023-04-21 10:44:27 +0900

    Footerコンポーネントを作成

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,32 @@
+import { Box, chakra, Flex, Link, Spacer, Stack, Text } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { CiTwitter } from 'react-icons/ci';
+
+export default function Footer() {
+  return (
+    <chakra.footer>
+      <Flex bg="blue.500" bottom={0} direction={'column'} pos="fixed" py={2} w="full">
+        <Stack align="center" direction={'column'} spacing={0}>
+          <Stack direction={'row'} spacing={2}>
+            <Link as={NextLink} href={'/terms'}>
+              利用規約
+            </Link>
+            <p> | </p>
+            <Link as={NextLink} href={'/privacy'}>
+              プライバシーポリシー
+            </Link>
+            <p> | </p>
+            <Link as={NextLink} href={'https://twitter.com/Utsubo256'} target="_blank">
+              <Box icon={<CiTwitter />}>お問い合わせ</Box>
+            </Link>
+          </Stack>
+          <Box mb={2}>
+            <Spacer />
+            <Text fontSize="sm">Copyright &copy; {new Date().getFullYear()} - All right reserved by Utsubo</Text>
+            <Spacer />
+          </Box>
+        </Stack>
+      </Flex>
+    </chakra.footer>
+  );
+}

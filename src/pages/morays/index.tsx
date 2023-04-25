1250df49db473d80e45f0076b7c59c4ccc2f07bc
Utsubo256 <greeeentea256@gmail.com>
2023-04-25 10:06:29 +0900

    ウツボ一覧ページを追加

---
+++ src/pages/morays/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+import { Center, Image, LinkBox, LinkOverlay, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react';
+import axios from 'axios';
+
+export default function Morays() {
+  const [morays, setMorays] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/morays').then((res) => setMorays(res.data));
+  }, []);
+
+  return (
+    <>
+      <Center py="25px">
+        <Text fontSize="3xl">ウツボ一覧</Text>
+      </Center>
+      <Wrap justify="center" pb={8} spacing={8}>
+        {morays.map((moray) => (
+          <WrapItem key={moray.id} mx="auto">
+            <LinkBox
+              _hover={{ cursor: 'pointer', opacity: 0.8 }}
+              bg="whiteAlpha.800"
+              borderRadius="30px"
+              h="300px"
+              p={4}
+              shadow="lg"
+              w="230px"
+            >
+              <Stack spacing={1} textAlign="center">
+                <Image alt="moray_image" borderRadius="full" boxSize="160px" m="auto" src={moray.avatar} />
+                <LinkOverlay fontSize="xl" fontWeight="bold" href={`/morays/${moray.id}`}>
+                  {moray.name_ja}
+                </LinkOverlay>
+                <Text>{moray.name_en}</Text>
+                <Text>最大長: {moray.max_length_str}</Text>
+              </Stack>
+            </LinkBox>
+          </WrapItem>
+        ))}
+      </Wrap>
+    </>
+  );
+}

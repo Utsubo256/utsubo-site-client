d1157ea2566c167d001956d5abf2f063eadbdbb7
Utsubo256 <greeeentea256@gmail.com>
2023-04-28 12:08:53 +0900

    フッターの文字色、テキストを修正

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <chakra.footer>
       <Flex bg="blue.500" bottom={0} direction={'column'} pos="absolute" py={2} w="full">
-        <Stack align="center" direction={'column'} spacing={0}>
+        <Stack align="center" color="whiteAlpha.900" direction={'column'} spacing={0}>
           <Stack direction={'row'} spacing={2}>
             <Link as={NextLink} href={'/terms'}>
               利用規約
@@ -21,7 +21,7 @@ export default function Footer() {
           </Stack>
           <Box mb={2}>
             <Spacer />
-            <Text fontSize="sm">Copyright &copy; {new Date().getFullYear()} - All right reserved by Utsubo</Text>
+            <Text fontSize="sm">Copyright &copy; {new Date().getFullYear()} - All right reserved by Utsubo256</Text>
             <Spacer />
           </Box>
         </Stack>

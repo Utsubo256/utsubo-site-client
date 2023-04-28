f3a3e1ff7b816498d84050300f25aad0548d33ec
Utsubo256 <greeeentea256@gmail.com>
2023-04-28 13:51:57 +0900

    Headerを上部に固定するよう修正

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -12,7 +12,6 @@ import {
   MenuItem,
   MenuDivider,
   useDisclosure,
-  useColorModeValue,
   Stack,
   Text,
   Link,
@@ -34,95 +33,98 @@ export default function Header() {
   const { currentUser, loading, logout, userInfo } = useAuthContext();
 
   return (
-    <Box bg={useColorModeValue('blue.500', 'blue.900')} px={4}>
-      <Flex alignItems={'center'} h={16} justifyContent={'space-between'}>
-        <IconButton
-          aria-label={'Open Menu'}
-          display={{ md: 'none' }}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          onClick={isOpen ? onClose : onOpen}
-          size={'md'}
-        />
-        <HStack alignItems={'center'} spacing={8}>
-          <NextLink href="/">
-            <Image alt="utsubo-site-logo" h="40px" src="utsubo-site-logo.png" w="160px" />
-          </NextLink>
-          <HStack as={'nav'} display={{ base: 'none', md: 'flex' }} spacing={4}>
-            {navbarLinks.map((link) => (
-              <Link
-                _hover={{
-                  bg: 'gray.200',
-                  textDecoration: 'none',
-                }}
-                as={NextLink}
-                color={'whiteAlpha.900'}
-                href={link.url}
-                key={link.name} // key propsはイテレータ内でどのアイテムかをReactが認識するために使われる
-                px={2}
-                py={1}
-                rounded={'md'}
-              >
-                {link.name}
-              </Link>
-            ))}
+    <>
+      <Box bg="blue.500" h="64px" position="fixed" px={4} w={'full'} zIndex={100}>
+        <Flex alignItems={'center'} h={16} justifyContent={'space-between'}>
+          <IconButton
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            onClick={isOpen ? onClose : onOpen}
+            size={'md'}
+          />
+          <HStack alignItems={'center'} spacing={8}>
+            <NextLink href="/">
+              <Image alt="utsubo-site-logo" h="40px" src="utsubo-site-logo.png" w="160px" />
+            </NextLink>
+            <HStack as={'nav'} display={{ base: 'none', md: 'flex' }} spacing={4}>
+              {navbarLinks.map((link) => (
+                <Link
+                  _hover={{
+                    bg: 'gray.200',
+                    textDecoration: 'none',
+                  }}
+                  as={NextLink}
+                  color={'whiteAlpha.900'}
+                  href={link.url}
+                  key={link.name} // key propsはイテレータ内でどのアイテムかをReactが認識するために使われる
+                  px={2}
+                  py={1}
+                  rounded={'md'}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </HStack>
           </HStack>
-        </HStack>
-        <Flex alignItems={'center'}>
-          <Menu>
-            {!loading && currentUser ? (
-              <>
-                <MenuButton as={Button} cursor={'pointer'} minW={0} rounded={'full'} variant={'link'}>
-                  <Avatar size={'sm'} src="default-user-icon.png" />
-                </MenuButton>
-                <MenuList>
-                  <MenuItem as="a" href="#">
-                    <Stack>
-                      <Text>ログインユーザー</Text>
-                      <Text as="b">{userInfo.name}</Text>
-                    </Stack>
-                  </MenuItem>
-                  <MenuDivider />
-                  <MenuItem as="a" href="##">
-                    プロフィール編集
-                  </MenuItem>
-                  <MenuDivider />
-                  <MenuItem icon={<BiLogOut />} onClick={() => logout()}>
-                    ログアウト
-                  </MenuItem>
-                </MenuList>
-              </>
-            ) : (
-              <Button as={NextLink} href="/signin">
-                ログイン
-              </Button>
-            )}
-          </Menu>
+          <Flex alignItems={'center'}>
+            <Menu>
+              {!loading && currentUser ? (
+                <>
+                  <MenuButton as={Button} cursor={'pointer'} minW={0} rounded={'full'} variant={'link'}>
+                    <Avatar size={'sm'} src="default-user-icon.png" />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem as="a" href="#">
+                      <Stack>
+                        <Text>ログインユーザー</Text>
+                        <Text as="b">{userInfo.name}</Text>
+                      </Stack>
+                    </MenuItem>
+                    <MenuDivider />
+                    <MenuItem as="a" href="##">
+                      プロフィール編集
+                    </MenuItem>
+                    <MenuDivider />
+                    <MenuItem icon={<BiLogOut />} onClick={() => logout()}>
+                      ログアウト
+                    </MenuItem>
+                  </MenuList>
+                </>
+              ) : (
+                <Button as={NextLink} href="/signin">
+                  ログイン
+                </Button>
+              )}
+            </Menu>
+          </Flex>
         </Flex>
-      </Flex>
 
-      {isOpen ? (
-        <Box display={{ md: 'none' }} pb={4}>
-          <Stack as={'nav'} spacing={4}>
-            {navbarLinks.map((link) => (
-              <Link
-                _hover={{
-                  bg: 'gray.200',
-                  textDecoration: 'none',
-                }}
-                as={NextLink}
-                color={'whiteAlpha.900'}
-                href={link.url}
-                key={link.name} // key propsはイテレータ内でどのアイテムかをReactが認識するために使われる
-                px={2}
-                py={1}
-                rounded={'md'}
-              >
-                {link.name}
-              </Link>
-            ))}
-          </Stack>
-        </Box>
-      ) : null}
-    </Box>
+        {isOpen ? (
+          <Box display={{ md: 'none' }} pb={4}>
+            <Stack as={'nav'} spacing={4}>
+              {navbarLinks.map((link) => (
+                <Link
+                  _hover={{
+                    bg: 'gray.200',
+                    textDecoration: 'none',
+                  }}
+                  as={NextLink}
+                  color={'whiteAlpha.900'}
+                  href={link.url}
+                  key={link.name} // key propsはイテレータ内でどのアイテムかをReactが認識するために使われる
+                  px={2}
+                  py={1}
+                  rounded={'md'}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+      <Box h="64px"></Box>
+    </>
   );
 }

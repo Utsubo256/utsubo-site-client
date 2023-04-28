7aac68b77b6eefe18f540015bd095324afbe5191
Utsubo256 <greeeentea256@gmail.com>
2023-04-28 12:15:37 +0900

    サインインページのh1部分、カード背景色を修正

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center, Container, Divider, HStack, Heading, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Container, Divider, HStack, Stack, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import { FcGoogle } from 'react-icons/fc';
 
@@ -29,37 +29,37 @@ export default function SigninPage() {
   };
 
   return (
-    <Container maxW="lg" px={{ base: '0', sm: '8' }} py={{ base: '12', md: '24' }}>
-      <Stack spacing="6">
-        <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-          <Heading size={{ base: 'xs', md: 'sm' }}>ログイン/ユーザー登録</Heading>
-        </Stack>
-      </Stack>
-      <Box
-        bg={{ base: 'transparent', sm: 'bg-surface' }}
-        borderRadius={{ base: 'none', sm: 'xl' }}
-        boxShadow={{ base: 'none', sm: 'md' }}
-        px={{ base: '4', sm: '10' }}
-        py={{ base: '0', sm: '8' }}
-      >
-        <Stack spacing="6">
-          <Button maxW={'md'} variant={'outline'} w={'full'}>
-            ゲストログイン
-          </Button>
-          <HStack>
-            <Divider />
-            <Text color="muted" fontSize="sm" whiteSpace="nowrap">
-              または
-            </Text>
-            <Divider />
-          </HStack>
-          <Button leftIcon={<FcGoogle />} maxW={'md'} onClick={() => handleLogin()} variant={'outline'} w={'full'}>
-            <Center>
-              <Text>Googleでログイン / ユーザー登録</Text>
-            </Center>
-          </Button>
-        </Stack>
-      </Box>
-    </Container>
+    <>
+      <Center pt="25px">
+        <Text fontSize="3xl">ログイン/ユーザー登録</Text>
+      </Center>
+      <Container maxW="lg" px={{ base: '0', sm: '8' }} py={12}>
+        <Box
+          bg="whiteAlpha.900"
+          borderRadius={{ base: 'none', sm: 'xl' }}
+          boxShadow={{ base: 'none', sm: 'md' }}
+          px={{ base: '4', sm: '10' }}
+          py={{ base: '0', sm: '8' }}
+        >
+          <Stack spacing="6">
+            <Button maxW={'md'} variant={'outline'} w={'full'}>
+              ゲストログイン
+            </Button>
+            <HStack>
+              <Divider />
+              <Text color="muted" fontSize="sm" whiteSpace="nowrap">
+                または
+              </Text>
+              <Divider />
+            </HStack>
+            <Button leftIcon={<FcGoogle />} maxW={'md'} onClick={() => handleLogin()} variant={'outline'} w={'full'}>
+              <Center>
+                <Text>Googleでログイン / ユーザー登録</Text>
+              </Center>
+            </Button>
+          </Stack>
+        </Box>
+      </Container>
+    </>
   );
 }
